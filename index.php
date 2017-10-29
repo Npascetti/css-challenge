2f81eb5ffd1b9874008509803aac54df5acd738d
Nicklas Pascetti <Npascetti@gmail.com>
2017-10-29 01:32:00 -0600

    added cousine font

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
+		<!--link to Google Fonts-->
+		<link href="https://fonts.googleapis.com/css?family=Cousine" rel="stylesheet">
+
+
+
 		<!-- custom CSS stylesheet -->
 		<link rel="stylesheet" href="css/style.css" type="text/css" />
 
